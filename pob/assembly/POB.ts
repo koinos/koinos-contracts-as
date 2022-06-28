c7b311b0259d16ce5d0d2e73f48fe8427178a6a4
Nathaniel Caldwell <nbcwell@gmail.com>
2022-06-28 18:07:30 -0400

    Work on pob

--- pob/assembly/POB.ts
+++ pob/assembly/POB.ts
@@ -1,4 +1,4 @@
-import { u256 } from "as-bignum";
+import { BigInt, Bytes } from '@graphprotocol/graph-ts'
 import { authority, chain, protocol, system_call_ids, System, Protobuf, 
     Base58, value, any, system_calls, Token, SafeMath } from "koinos-sdk-as";
 
@@ -16,18 +16,17 @@ namespace Constants {
   export const METADATA_KEY: Uint8Array = new Uint8Array(0);
   export const INITIAL_DIFFICULTY_BITS:u32 = 32;
   export const TARGET_BLOCK_INTERVAL_S:u32 = 10;
-  export const BLOCK_TIME_QUANTA = 10;
+  export const BLOCK_QUANTA :u32 = 10;
 
   export const VHP_DEDUCTION:u64 =  95000000
   export const BLOCK_REWARD:u64  = 100000000
-
 }
 
 export class POB {
   register_public_key(args: pob.register_public_key_arguments): pob.register_public_key_result {
-    const registration = System.getObject<Uint8Array, pob.address_record>(State.Space.REGISTRATION, args.public_key!, pob.address_record.decode);
+    const db = System.getObject<Uint8Array, pob.address_record>(State.Space.REGISTRATION, args.public_key!, pob.address_record.decode);
     
-    if (registration) {
+    if (db) {
       System.log("Public key: '" + args.public_key + "' already registered. Overwriting.");
     }
 
@@ -37,7 +36,7 @@ export class POB {
 
     // Create and store the record
     const record = new pob.address_record(sender);
-    System.putObject(State.Space.REGISTRATION, args.public_key!, record, pob.address_record.encode);
+    System.putObject(State.Space.REGISTRATION, args.public_key!, record, pob.public_key_record.encode);
 
     // Emit an event
     const event = new pob.register_public_key_event(sender, args.public_key);
@@ -64,24 +63,31 @@ export class POB {
   process_block_signature(args: system_calls.process_block_signature_arguments): system_calls.process_block_signature_result {
     const sig_data_bytes = args.signature!;
     const signature = Protobuf.decode<pob.signature_data>(sig_data_bytes, pob.signature_data.decode);
-
     const signer = args.header!.signer!;
 
     const token = new Token(Constants.TOKEN_CONTRACT_ID);
     const vhp = new Token(Constants.VHP_CONTRACT_ID);
+
+    const metadata = this.fetch_metadata();
     
-    // Check block time against time quanta
+    // Check block quanta
+    System.require
 
     // Check vrf proof against seed and block time
-    const registration = System.getObject<Uint8Array, pob.address_record>(State.Space.REGISTRATION, args.public_key!, pob.address_record.decode);
-
+    //System.verifyVRFProof(publicKey: Uint8Array, proof: Uint8Array, hash: Uint8Array, message: Uint8Array, type: chain.dsa = chain.dsa.ecdsa_secp256k1): bool {
+      const registration = System.getObject<Uint8Array, pob.public_key_record>(State.Space.REGISTRATION, signer, pob.public_key_record.decode);
+      System.require(registration, "signer address has no public key record");
+    System.verifyVRFProof()
     // Ensure vrf hash divided by producer's vhp is below difficulty
-    const metadata = this.fetch_metadata();
-    System.require(signature.vrf_hash / vhp.balanceOf(signer) < metadata.difficulty, "provided hash is not sufficient");
+    const difficulty = BigInt.fromUnsignedBytes(Bytes.fromUint8Array(metadata.metadata!.difficulty!));
+    const hash = BigInt.fromUnsignedBytes(Bytes.fromUint8Array(signature.vrf_hash!));
+    const vhp_balance = BigInt.fromString(vhp.balanceOf(signer).toString()); // TODO: This is very ugly
+
+    System.require(hash/vhp_balance < difficulty, "provided hash is not sufficient");
 
     // On successful block deduct vhp and mint new koin
-    System.require(token.mint(signer, Constants.BLOCK_REWARD));
-    System.require(vhp.burn(signer, Constants.VHP_DEDUCTION));
+    System.require(vhp.burn(signer, Constants.VHP_DEDUCTION), "could not burn vhp");
+    System.require(token.mint(signer, Constants.BLOCK_REWARD), "could not mint token");
 
     this.update_difficulty();
 
@@ -109,12 +115,12 @@ export class POB {
     
     var difficulty = new u256();
     var seed = new u256();
+    var a = 
 
     new_data.difficulty = difficulty.toBytes();
     new_data.seed = seed.toBytes();
     new_data.last_block_time = System.getHeadInfo().head_block_time;
     new_data.target_block_interval = Constants.TARGET_BLOCK_INTERVAL_S;
-    // var a = seed * difficulty;
 
     // Store it
     System.putObject(State.Space.METADATA, Constants.METADATA_KEY, new_data, pob.metadata.encode);
