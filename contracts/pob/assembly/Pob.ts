8391299ff98952bd69df68f89cd63de36c1f4bcf
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2022-07-28 09:50:46 -0700

    Fix precision, u64 overflow, authorization, and minor performance improvements

--- contracts/pob/assembly/Pob.ts
+++ contracts/pob/assembly/Pob.ts
@@ -15,14 +15,14 @@ namespace Constants {
    *
    *  x = blocks_per_year * ( ( 1 + desired_inflation_rate ) ^ ( 1 / blocks_per_year ) - 1 )
    */
-  export const DEFAULT_ANNUAL_INFLATION_RATE: u32 = 198; // 2%
-  export const DEFAULT_TARGET_BURN_PERCENT: u32 = 50100; // 50.1%
+  export const DEFAULT_ANNUAL_INFLATION_RATE: u32 = 1980; // 2%
+  export const DEFAULT_TARGET_BURN_PERCENT: u32 = 501000; // 50.1%
   export const DEFAULT_TARGET_BLOCK_INTERVAL_MS: u32 = 3000; // 3s
   export const DEFAULT_QUANTUM_LENGTH_MS: u32 = 10;
   export const METADATA_KEY: Uint8Array = new Uint8Array(0);
   export const CONSENSUS_PARAMS_KEY: Uint8Array = new Uint8Array(1);
   export const INITIAL_DIFFICULTY_BITS:u8 = 48;
-  export const FIXED_POINT_PRECISION :u32 = 1000;
+  export const ONE_HUNDRED_PERCENT: u32 = 1000000;
   export const MILLISECONDS_PER_YEAR = 31536000000;
 
   let contractId: Uint8Array | null = null;
@@ -50,11 +50,9 @@ namespace Constants {
     if (koinContractId === null) {
       if (BUILD_FOR_TESTING) {
         // Address: BRmrUgtSQVUggoeE9weG4f7nidyydnYfQ
-        //koinContractId = Arrays.fromHexString("007260aeafadc70431ea9c3fbef135b9a415c10f5195e8d557");
         koinContractId = arrayToUint8Array([0x00, 0x72, 0x60, 0xae, 0xaf, 0xad, 0xc7, 0x04, 0x31, 0xea, 0x9c, 0x3f, 0xbe, 0xf1, 0x35, 0xb9, 0xa4, 0x15, 0xc1, 0x0f, 0x51, 0x95, 0xe8, 0xd5, 0x57]);
       } else {
         // Address: 19JntSm8pSNETT9aHTwAUHC5RMoaSmgZPJ
-        //koinContractId = Arrays.fromHexString("005b1e61d37259b9c2d99bf417f592e0b77725165d2488be45");
         koinContractId = arrayToUint8Array([0x00, 0x5b, 0x1e, 0x61, 0xd3, 0x72, 0x59, 0xb9, 0xc2, 0xd9, 0x9b, 0xf4, 0x17, 0xf5, 0x92, 0xe0, 0xb7, 0x77, 0x25, 0x16, 0x5d, 0x24, 0x88, 0xbe, 0x45]);
       }
     }
@@ -66,11 +64,9 @@ namespace Constants {
     if (vhpContractId === null ) {
       if (BUILD_FOR_TESTING) {
         // Address: 1CZvRyRuNxghMUUNGqsKsT5x55r6wugd1C
-        //vhpContractId = Arrays.fromHexString("007ee34bc608c04cd537347cf7302815774fcf750c6a8775f3");
         vhpContractId = arrayToUint8Array([0x00, 0x7e, 0xe3, 0x4b, 0xc6, 0x08, 0xc0, 0x4c, 0xd5, 0x37, 0x34, 0x7c, 0xf7, 0x30, 0x28, 0x15, 0x77, 0x4f, 0xcf, 0x75, 0x0c, 0x6a, 0x87, 0x75, 0xf3]);
       } else {
         // Address: 1JZqj7dDrK5LzvdJgufYBJNUFo88xBoWC8
-        // vhpContractId = Arrays.fromHexString("00c0b01fa4bbcd9f061e0df292670ca0dbfaa6526adb88ae09");
         vhpContractId = arrayToUint8Array([0x00, 0xc0, 0xb0, 0x1f, 0xa4, 0xbb, 0xcd, 0x9f, 0x06, 0x1e, 0x0d, 0xf2, 0x92, 0x67, 0x0c, 0xa0, 0xdb, 0xfa, 0xa6, 0x52, 0x6a, 0xdb, 0x88, 0xae, 0x09]);
       }
     }
@@ -138,11 +134,9 @@ export class Pob {
   }
 
   process_block_signature(args: system_calls.process_block_signature_arguments): system_calls.process_block_signature_result {
-    const sig_data_bytes = args.signature!;
-    const signature = Protobuf.decode<pob.signature_data>(sig_data_bytes, pob.signature_data.decode);
-    const signer = args.header!.signer!;
-    const timestamp = args.header!.timestamp;
-    const head_block_time = System.getHeadInfo().head_block_time;
+    System.require(System.getCaller().caller_privilege == chain.privilege.kernel_mode, 'pob contract process block signature must be called from kernel');
+
+    const signature = Protobuf.decode<pob.signature_data>(args.signature!, pob.signature_data.decode);
 
     const koin = new Token(Constants.KoinContractId());
     const vhp = new Token(Constants.VhpContractId());
@@ -154,11 +148,11 @@ export class Pob {
     System.require(args.header!.timestamp % params.quantum_length == 0, "time stamp does not match time quanta");
 
     // Get signer's public key
-    const registration = System.getObject<Uint8Array, pob.public_key_record>(State.Space.Registration(), signer, pob.public_key_record.decode);
+    const registration = System.getObject<Uint8Array, pob.public_key_record>(State.Space.Registration(), args.header!.signer!, pob.public_key_record.decode);
     System.require(registration != null, "signer address has no public key record");
 
     // Create vrf payload and serialize it
-    const payload = new pob.vrf_payload(metadata.seed, timestamp);
+    const payload = new pob.vrf_payload(metadata.seed, args.header!.timestamp);
     const payload_bytes = Protobuf.encode(payload, pob.vrf_payload.encode);
 
     System.require(System.verifyVRFProof(registration!.public_key!, signature.vrf_proof!, signature.vrf_hash!, payload_bytes), "proof failed vrf validation");
@@ -170,25 +164,26 @@ export class Pob {
     mh.deserialize(signature.vrf_hash!)
     // This purposefully only gets the upper 128 bits. That's all we need
     let hash = u128.fromBytes(mh.digest, true);
-    const vhp_balance = u128.fromU64(vhp.balanceOf(signer));
+    const vhp_balance = u128.fromU64(vhp.balanceOf(args.header!.signer!));
 
     hash = hash / vhp_balance;
 
     System.require(hash < target, "provided hash is not sufficient");
 
-    const virtual_supply = koin.totalSupply() + vhp.totalSupply();
-    const yearly_inflation = virtual_supply * params.target_annual_inflation_rate / Constants.FIXED_POINT_PRECISION;
+    const virtual_supply = u128.fromU64(koin.totalSupply() + vhp.totalSupply());
+    const yearly_inflation = virtual_supply * u128.fromU64(params.target_annual_inflation_rate) / u128.fromU64(Constants.ONE_HUNDRED_PERCENT);
 
     const blocks_per_year = Constants.MILLISECONDS_PER_YEAR / params.target_block_interval;
 
-    const block_reward = yearly_inflation / blocks_per_year;
-    const vhp_reduction = (virtual_supply * params.target_burn_percent) / (blocks_per_year * Constants.FIXED_POINT_PRECISION);
+    const block_reward = yearly_inflation.toU64() / blocks_per_year;
+    // virtual supply starts at 10000000000000000 and target_burn_percent is 501000. This already uses 73 bits
+    const vhp_reduction = (virtual_supply * u128.fromU64(params.target_burn_percent)) / u128.fromU64(blocks_per_year * Constants.ONE_HUNDRED_PERCENT);
 
     // On successful block deduct vhp and mint new koin
-    System.require(vhp.burn(signer, vhp_reduction), "could not burn vhp");
-    System.require(koin.mint(signer, block_reward + vhp_reduction), "could not mint token");
+    System.require(vhp.burn(args.header!.signer!, vhp_reduction.toU64()), "could not burn vhp");
+    System.require(koin.mint(args.header!.signer!, block_reward + vhp_reduction.toU64()), "could not mint token");
 
-    this.update_difficulty(difficulty, metadata, params, head_block_time, signature.vrf_hash!);
+    this.update_difficulty(difficulty, metadata, params, args.header!.timestamp, signature.vrf_hash!);
 
     return new system_calls.process_block_signature_result(true);
   }
@@ -200,7 +195,7 @@ export class Pob {
 
   update_difficulty(difficulty:u128, metadata:pob.metadata, params:pob.consensus_parameters, current_block_time:u64, vrf_hash:Uint8Array): void {
     // This is a generalization of Ethereum's Homestead difficulty adjustment algorithm
-    let block_time_denom = (10000 * params.target_block_interval) / 14000;
+    const block_time_denom = (10000 * params.target_block_interval) / 14000;
 
     // Calulate new difficulty
     let new_difficulty: u128 = difficulty / u128.fromU64(2048);
@@ -212,10 +207,11 @@ export class Pob {
       new_difficulty = difficulty - new_difficulty * u128.fromU64(u64(-multiplier));
     }
 
-    let new_data = new pob.metadata();
-    new_data.difficulty = new_difficulty.toUint8Array(true);
-    new_data.seed = System.hash(Crypto.multicodec.sha2_256, vrf_hash);
-    new_data.last_block_time = current_block_time;
+    let new_data = new pob.metadata(
+      System.hash(Crypto.multicodec.sha2_256, vrf_hash),
+      new_difficulty.toUint8Array(true),
+      current_block_time
+    );
 
     System.putObject(State.Space.Metadata(), Constants.METADATA_KEY, new_data, pob.metadata.encode);
   }
