21eb2cda4143c2945204fafebd2f4c698ca0d147
Steve Gerbino <steve@gerbino.co>
2022-09-19 12:53:38 -0400

    Use the new packages to build the claim contract

--- contracts/claim/assembly/Claim.ts
+++ contracts/claim/assembly/Claim.ts
@@ -1,4 +1,4 @@
-import { chain, System, Base58, Token, Crypto, claim, Arrays, StringBytes } from "koinos-sdk-as";
+import { chain, System, Base58, Token, Crypto, claim, Arrays, StringBytes } from "@koinos/sdk-as";
 
 function arrayToUint8Array(a: Array<u8>): Uint8Array {
   let uArray = new Uint8Array(a.length);
