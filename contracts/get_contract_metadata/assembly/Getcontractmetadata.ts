eee9ea3e6809ecad3e8782d9ac0b7c29a5bc9e40
Roamin <roamin@tuta.io>
2023-05-23 04:04:04 +0000

    simplify contract

--- contracts/get_contract_metadata/assembly/Getcontractmetadata.ts
+++ contracts/get_contract_metadata/assembly/Getcontractmetadata.ts
@@ -5,23 +5,15 @@ export class Getcontractmetadata {
   get_contract_metadata(
     args: getcontractmetadata.get_contract_metadata_arguments
   ): getcontractmetadata.get_contract_metadata_result {
-    const contract_id = args.contract_id;
-
-    const res = new getcontractmetadata.get_contract_metadata_result();
-
     const contractMetadataSpace = new chain.object_space(
       true, 
-      // kernel zone is 
+      // kernel zone is "empty bytes"
       new Uint8Array(0), 
       object_spaces.system_space_id.contract_metadata
     );
 
-    const metadataObj = System.getObject<Uint8Array, chain.contract_metadata_object>(contractMetadataSpace, contract_id, chain.contract_metadata_object.decode);
-
-    if (metadataObj) {
-      res.contract_metadata = metadataObj;
-    }
-
-    return res;
+    return new getcontractmetadata.get_contract_metadata_result(
+      System.getObject<Uint8Array, chain.contract_metadata_object>(contractMetadataSpace, args.contract_id, chain.contract_metadata_object.decode)
+    );
   }
 }
